f9594fcbbaccbbc6e656d5f63bf53127b34063a0
acciarric <crisbt7@gmail.com>
2025-01-29 12:11:36 +0000

    Deploying to gh-pages from @ acciarric/acciarric.github.io@5016c565f6556a3201fb9271b6558a669cc81718 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-miscellanea",
           title: "miscellanea",
-          description: "A growing collection of miscellaneous items, as pictures, some useful links and videos (UNDER CONSTRUCTION ‌)",
+          description: "A growing collection of miscellaneous items, as pictures, some useful links and videos ‌",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
